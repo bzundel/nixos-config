13227316a1fd28143c2b93abf660edeb812ef5c1
Benedikt Zundel <benedikt.zundel@live.com>
2024-10-03 03:35:55 +0200

    initial commit

---
+++ home-manager/home.nix
@@ -0,0 +1,290 @@
+{ config, pkgs, ... }:
+
+let
+  unstable = import <nixos-unstable> { config = config.nixpkgs.config; };
+in
+{
+  home.username = "bened";
+  home.homeDirectory = "/home/bened";
+
+  home.stateVersion = "24.05";
+
+  home.packages = with pkgs; [
+    #meta
+    ubuntu_font_family
+
+    #development
+    jetbrains.rider
+    pgadmin4-desktopmode
+    direnv
+    file
+    mono
+    omnisharp-roslyn
+    dotnet-sdk_8
+    ghc
+
+    #communication
+    evolution
+    signal-desktop
+    discord
+    element-desktop
+    simplex-chat-desktop
+
+    #entertainment
+    spotify
+    calibre
+
+    #utilities
+    keepassxc
+    obsidian
+    mpv
+    yt-dlp
+    rclone
+    xclip
+    unzip
+    wineWowPackages.stable
+    winetricks
+    remmina
+    chromium
+    kdePackages.kleopatra
+    fzf
+    thunderbird
+
+    #games
+    steam
+    starsector
+    xonotic
+    openttd
+    lutris
+    nudoku
+    prismlauncher
+    
+    #gnome
+    gnome.gnome-tweaks
+  ];
+
+  programs.gnome-dash-to-dock = {
+    enable = true;
+    extendHeight = true;
+    dashMaxIconSize = 32;
+    disableOverviewOnStartup = true;
+    dockFixed = true;
+    dockPosition = "LEFT";
+    hotKeys = false;
+    showMounts = false;
+    showMountsNetwork = false;
+    showTrash = false;
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+
+    shellAliases = {
+      hmconfig = "vim ${config.xdg.configHome}/home-manager/home.nix";
+      hmupdate = "home-manager switch";
+      nixconfig = "sudo vim /etc/nixos/configuration.nix";
+      nixupdate = "sudo nixos-rebuild switch";
+      deinit = "echo 'use flake' > .envrc; direnv allow";
+    };
+
+    initExtra = ''
+      eval "$(direnv hook zsh)"
+    '';
+
+    history = {
+      size = 10000;
+      path = "${config.xdg.dataHome}/zsh/history";
+    };
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" ];
+      theme = "half-life";
+    };
+  };
+
+  programs.tmux = {
+    enable = true;
+    terminal = "screen-256color";
+  };
+
+  programs.vim = 
+  let
+    omnisharp-vim = pkgs.vimUtils.buildVimPlugin {
+      name = "omnisharp-vim";
+      src = pkgs.fetchFromGitHub {
+        owner = "OmniSharp";
+        repo = "omnisharp-vim";
+        rev = "f9c5d3e3375e8b5688a4506e813cb21bdc7329b1";
+        hash = "sha256-z3Dgrm9pNWkvfShPmB9O8TqpY592sk1W722zduOSing=";
+      };
+    };   
+  in
+  {
+    enable = true;
+    defaultEditor = true;
+
+    plugins = with pkgs.vimPlugins; [
+      vim-nix
+      vim-lastplace
+      YouCompleteMe
+      ReplaceWithRegister
+      vim-peekaboo
+      nerdtree
+      vim-polyglot
+      #omnisharp-vim # TODO fix this plugin?
+    ];
+
+    extraConfig = ''
+      set nocompatible
+      set backspace=indent,eol,start
+      syntax on
+      set number relativenumber
+      set scrolloff=10
+      set wrap
+      set hlsearch
+      set incsearch
+      set showcmd
+      set showmode
+      set showmatch
+      set background=dark
+
+      let mapleader=","
+
+      nnoremap <leader>, ``
+      map <space> /
+
+      nnoremap <leader>n :NERDTreeFocus<CR>
+      nnoremap <C-n> :NERDTree<CR>
+      nnoremap <C-t> :NERDTreeToggle<CR>
+      nnoremap <C-f> :NERDTreeFind<CR>
+
+      autocmd StdinReadPre * let s:std_in=1
+
+      autocmd VimEnter * if argc() == 0 && !exists('s:std_in') | NERDTree | endif
+      autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | call feedkeys(":quit\<CR>:\<BS>") | endif
+    '';
+  };
+
+  programs.vscode = {
+    enable = true;
+
+    extensions = with pkgs.vscode-extensions; [
+      vscodevim.vim
+      ms-dotnettools.csharp
+      ms-dotnettools.vscodeintellicode-csharp
+      ms-dotnettools.csdevkit
+    ];
+  };
+
+  programs.git = {
+    enable = true;
+    userEmail = "benedikt.zundel@live.com";
+    userName = "Benedikt Zundel";
+
+    extraConfig = {
+      credential.helper = "${
+        pkgs.git.override { withLibsecret = true; }
+      }/bin/git-credential-libsecret";
+    };
+  };
+
+  programs.gnome-shell = {
+    enable = true;
+
+    theme = {
+      name = "Yaru-purple-dark";
+      package = pkgs.yaru-theme;
+    };
+
+    extensions = [
+      { package = pkgs.gnomeExtensions.dash-to-dock; }
+      { package = pkgs.gnomeExtensions.vitals; }
+      { package = unstable.gnomeExtensions.systemd-manager; }
+    ];
+  };
+
+  programs.gnome-terminal = {
+    enable = true;
+
+    profile."d389d7f9-374e-49ad-9230-09407436358f" = {
+      default = true;
+      visibleName = "Default";
+      allowBold = true;
+      audibleBell = false;
+      boldIsBright = true;
+      font = "Ubuntu Mono Bold 12";
+    };
+  };
+
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  home.sessionVariables = {
+    EDITOR = "vim";
+  };
+
+  gtk = {
+    enable = true;
+
+    cursorTheme = {
+      package = pkgs.yaru-theme;
+      name = "Yaru";
+    };
+
+    font = {
+      package = pkgs.ubuntu_font_family;
+      name = "Ubuntu";
+      size = 11;
+    };
+
+    iconTheme = {
+      package = pkgs.yaru-theme;
+      name = "Yaru-purple-dark";
+    };
+
+    theme = {
+      package = pkgs.yaru-theme;
+      name = "Yaru-purple-dark";
+    };
+  };
+
+  systemd.user.services.rclone-onedrive = {
+    Unit = {
+      Description = "Mount OneDrive to home using rclone";
+    };
+
+    Service = 
+    let 
+      onedriveDir = "/home/bened/cloud";
+    in
+    {
+      Type = "simple";
+      ExecStart = "${pkgs.rclone}/bin/rclone mount --vfs-cache-mode writes onedrive:/Personal/KeePassDatabase ${onedriveDir}";
+      ExecStop = "/run/wrappers/bin/fusermount -u ${onedriveDir}";
+      Restart = "on-failure";
+      RestartSec = "10";
+    };
+  };
+
+  nix = {
+    package = pkgs.nix;
+    settings.experimental-features = [ "nix-command" "flakes" ];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  programs.home-manager.enable = true;
+  programs.home-manager.path = "$HOME/repos/home-manager";
+}
---
+++ nixos/configuration.nix
@@ -0,0 +1,161 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      /etc/nixos/hardware-configuration.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "cora";
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  networking.networkmanager = {
+    enable = true;
+    dns = "default";
+  };
+  
+  # TODO un-hardcode path to ovpn file
+  services.openvpn.servers = {
+    rz_muenster = {
+      config = ''
+        config /home/bened/Documents/Work/RZ_Muenster.ovpn
+      '';
+      autoStart = false;
+      updateResolvConf = true;
+    };
+  };
+
+  time.timeZone = "Europe/Berlin";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  services.xserver.enable = true;
+
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.wayland = false;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.videoDrivers = [ "intel" ];
+
+  environment.gnome.excludePackages = (with pkgs; [
+    gnome-photos
+    gnome-tour
+    gedit
+  ]) ++ (with pkgs.gnome; [
+    cheese
+    gnome-music
+    epiphany
+    geary
+    evince
+    totem
+  ]);
+
+  services.xserver.xkb = {
+    layout = "us,de";
+    variant = "";
+  };
+
+  services.printing.enable = true;
+
+  services.udisks2.enable = true; # for calibre
+
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  hardware.opengl.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  users.users.bened = {
+    isNormalUser = true;
+    description = "Benedikt Zundel";
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.zsh;
+    packages = [ ];
+  };
+
+  programs.firefox.enable = true;
+
+  programs.zsh.enable = true;
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+  };
+
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+
+  services.postgresql = {
+    enable = true;
+    authentication = ''
+      local all all trust
+    '';
+  };
+
+  environment.systemPackages = with pkgs; [
+    git
+    wget
+    vim
+    gnome.gnome-terminal 
+
+    hunspell
+    hunspellDicts.en_US
+    hunspellDicts.de_DE
+  ];
+
+  security.wrappers = {
+    fusermount.source = "${pkgs.fuse}/bin/fusermount";
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
