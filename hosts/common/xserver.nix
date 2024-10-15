3198e32fbbff15e4b3b719c68e084ecadb32de9b
Benedikt Zundel <benedikt.zundel@live.com>
2024-10-15 02:47:47 +0200

    sys: remove xmonad

--- hosts/common/xserver.nix
+++ hosts/common/xserver.nix
@@ -4,11 +4,6 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.gdm.wayland = false;
 
-  #services.xserver.windowManager.xmonad = {
-  #  enable = true;
-  #  enableContribAndExtras = true;
-  #};
-
   services.xserver.desktopManager.gnome.enable = true;
 
   services.xserver.videoDrivers = [ "intel" ];
