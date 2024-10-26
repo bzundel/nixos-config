14d6e1b8b6158f205c0fb625c0d529984a0f5a91
Benedikt Zundel <benedikt.zundel@live.com>
2024-10-26 10:47:20 +0200

    feat: rewrite and redocumentation of gnome-dash-to-dock module
    Organize options in sensible submodules to make the module more
    extensible and clean. Also rewrote all option descriptions.

--- modules/home-manager/gnome-dash-to-dock.nix
+++ modules/home-manager/gnome-dash-to-dock.nix
@@ -1,4 +1,3 @@
-# FIXME rewrite the descriptions
 {
   config,
   lib,
@@ -10,112 +9,136 @@ let
   cfg = config.programs.gnome-dash-to-dock;
 in
 {
+  meta.maintainers = [ maintainers.bzundel ];
+
   options.programs.gnome-dash-to-dock = {
     enable = mkEnableOption "GNOME dash to dock extension customization";
 
-    extendHeight = mkOption {
-      type = types.bool;
-      default = false;
-      description = ''
-        Whether or not to use panel mode.
-      '';
+    appearance = mkOption {
+      description = "Configure the dock appearance.";
+      type = types.submodule {
+
+        options = {
+          extendHeight = mkOption {
+            type = types.bool;
+            default = false;
+            description = ''
+              Extends the dock to the full height of the screen (panel mode).
+            '';
+          };
+
+          dockPosition = mkOption {
+            type = types.str;
+            default = "LEFT";
+            example = "BOTTOM";
+            description = ''
+              Sets the position of the dock. One of "LEFT", "TOP", "RIGHT" or "BOTTOM".
+            '';
+          };
+
+          applyCustomTheme = mkOption {
+            type = types.bool;
+            default = false;
+            description = ''
+              Enables the use of a custom theme for the dock.
+            '';
+          };
+
+          shrinkDash = mkOption {
+            type = types.bool;
+            default = false;
+            description = ''
+              Shrinks the dash icons to save space.
+            '';
+          };
+
+          dashMaxIconSize = mkOption {
+            type = types.int;
+            default = 48;
+            description = ''
+              Sets the maximum application icon size in the dock.
+            '';
+          };
+        };
+      };
     };
 
-    dockPosition = mkOption {
-      type = types.str;
-      default = "LEFT";
-      example = "BOTTOM";
-      description = ''
-        Position of the dock. One of "LEFT", "TOP", "RIGHT" or "BOTTOM".
-      '';
+    behavior = mkOption {
+      description = "Configure the dock behavior.";
+      type = types.submodule {
+        options = {
+          dockFixed = mkOption {
+            type = types.bool;
+            default = true;
+            description = ''
+              Keeps the dock visible at all times instead of using intelligent autohide.
+            '';
+          };
+
+          hotKeys = mkOption {
+            type = types.bool;
+            default = true;
+            description = ''
+              Enables using Super+{0..9} to launch applications.
+            '';
+          };
+
+          disableOverviewOnStartup = mkOption {
+            type = types.bool;
+            default = false;
+            description = ''
+              Prevents GNOME from showing overview on startup.
+            '';
+          };
+        };
+      };
     };
 
-    dockFixed = mkOption {
-      type = types.bool;
-      default = true;
-      description = ''
-        Whether to fix the dock position or use intelligent autohide.
-      '';
-    };
+    show = mkOption {
+      description = "Configure showing various shortcuts in the dock.";
+      type = types.submodule {
+        options = {
+          mounts = mkOption {
+            type = types.bool;
+            default = true;
+            description = ''
+              Show mounted drive shortcuts in the dock.
+            '';
+          };
 
-    hotKeys = mkOption {
-      type = types.bool;
-      default = true;
-      description = ''
-        Use Super+{0..9} to launch applications.
-      '';
-    };
+          mountsNetwork = mkOption {
+            type = types.bool;
+            default = true;
+            description = ''
+              Show mounted network drive shortcuts in the dock.
+            '';
+          };
 
-    applyCustomTheme = mkOption {
-      type = types.bool;
-      default = false;
-      description = ''
-        Whether to use built-in theme.
-      '';
-    };
-
-    shrinkDash = mkOption {
-      type = types.bool;
-      default = false;
-      description = ''
-        Shrink the dash icons to save space.
-      '';
-    };
-
-    disableOverviewOnStartup = mkOption {
-      type = types.bool;
-      default = false;
-      description = ''
-        Disable showing overview on startup.
-      '';
-    };
-
-    dashMaxIconSize = mkOption {
-      type = types.int;
-      default = 48;
-      description = ''
-        Maximum size of the icons in dock.
-      '';
-    };
-
-    showMounts = mkOption {
-      type = types.bool;
-      default = true;
-      description = ''
-        Whether or not to show mounted drives in the dock.
-      '';
-    };
-
-    showMountsNetwork = mkOption {
-      type = types.bool;
-      default = true;
-      description = ''
-        Whether or not to show mounted network drives in the dock.
-      '';
-    };
-
-    showTrash = mkOption {
-      type = types.bool;
-      default = true;
-      description = ''
-        Whether or not to show a trash icon in the dock.
-      '';
+          trash = mkOption {
+            type = types.bool;
+            default = true;
+            description = ''
+              Show trash shortcut in the dock.
+            '';
+          };
+        };
+      };
     };
   };
 
   config = mkIf cfg.enable {
     dconf.settings."org/gnome/shell/extensions/dash-to-dock" = {
-      extend-height = cfg.extendHeight;
-      dock-fixed = cfg.dockFixed;
-      dock-position = cfg.dockPosition;
-      hot-keys = cfg.hotKeys;
-      apply-custom-theme = cfg.applyCustomTheme;
-      custom-theme-shrink = cfg.shrinkDash;
-      disable-overview-on-startup = cfg.disableOverviewOnStartup;
-      dash-max-icon-size = cfg.dashMaxIconSize;
-      show-mounts = cfg.showMounts;
-      show-mounts-network = cfg.showMountsNetwork;
-      show-trash = cfg.showTrash;
+      extend-height = cfg.appearance.extendHeight;
+      dock-fixed = cfg.behavior.dockFixed;
+      dock-position = cfg.appearance.dockPosition;
+      hot-keys = cfg.behavior.hotKeys;
+      apply-custom-theme = cfg.appearance.applyCustomTheme;
+      custom-theme-shrink = cfg.appearance.shrinkDash;
+      disable-overview-on-startup = cfg.behavior.disableOverviewOnStartup;
+      dash-max-icon-size = cfg.appearance.dashMaxIconSize;
+      show-mounts = cfg.show.mounts;
+      show-mounts-network = cfg.show.mountsNetwork;
+      show-trash = cfg.show.trash;
     };
 
     home.packages = [ pkgs.gnomeExtensions.dash-to-dock ];
