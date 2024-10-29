0cfb58cddad48e6447fb331901a1656fb5df214c
Benedikt Zundel <benedikt.zundel@live.com>
2024-10-29 17:06:06 +0100

    feat: working configuration of dwm
    Includes sxhkd and slstatus

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -8,7 +8,6 @@
 }:
 let
   unstable = inputs.nixpkgs-unstable;
-  #swaybarStatus = pkgs.writeShellScriptBin "status.sh" (builtins.readFile ../config/sway/status.sh); # this seems weird and redundant
 in
 {
   imports = [ outputs.homeManagerModules.gnome-dash-to-dock ];
@@ -82,11 +81,15 @@ in
     #gnome
     gnome.gnome-tweaks
 
-    #sway
-    swaylock
-    swayidle
-    wmenu
+    #dwm
+    dmenu
+    (pkgs.slstatus.override {
+      conf = builtins.readFile ../config/slstatus/config.h;
+    })
+    slock
     st
+    sxhkd
+    xorg.xf86inputsynaptics # syndaemon
   ];
 
   programs.gnome-dash-to-dock = {
@@ -264,6 +267,15 @@ in
     ".vdirsyncer/config" = {
       source = ../config/vdirsyncer/config;
     };
+
+    ".dwm/autostart.sh" = {
+      source = ../config/dwm/autostart.sh;
+      executable = true;
+    };
+
+    ".config/sxhkd/sxhkdrc" = {
+      source = ../config/sxhkd/sxhkdrc;
+    };
   };
 
   home.sessionVariables = {
@@ -292,135 +304,6 @@ in
     theme = {
       package = pkgs.yaru-theme;
       name = "Yaru-purple-dark";
-    };
-  };
-
-  wayland.windowManager.sway = {
-    enable = true;
-    config = rec {
-      modifier = "Mod1";
-      terminal = "st";
-
-      left = "h";
-      down = "j";
-      up = "k";
-      right = "l";
-
-      defaultWorkspace = "workspace number 1";
-
-      keybindings = with config.wayland.windowManager.sway.config;
-      let
-        mod = modifier;
-      in {
-        "${mod}+Shift+Return" = "exec ${terminal}";
-        "${mod}+p" = "exec ${menu}";
-
-        "${mod}+Shift+c" = "reload";
-        "${mod}+Shift+q" = "kill";
-        "${mod}+Shift+e" = "exec swaymsg exit";
-
-        "${mod}+f" = "fullscreen";
-        "${mod}+e" = "layout toggle split";
-        "${mod}+s" = "layout stacking";
-        "${mod}+w" = "layout tabbed";
-
-        "${mod}+${left}" = "focus left";
-        "${mod}+${down}" = "focus down";
-        "${mod}+${up}" = "focus up";
-        "${mod}+${right}" = "focus right";
-
-        "${mod}+Shift+${left}" = "move left";
-        "${mod}+Shift+${down}" = "move down";
-        "${mod}+Shift+${up}" = "move up";
-        "${mod}+Shift+${right}" = "move right";
-
-        "${mod}+1" = "workspace number 1";
-        "${mod}+2" = "workspace number 2";
-        "${mod}+3" = "workspace number 3";
-        "${mod}+4" = "workspace number 4";
-        "${mod}+5" = "workspace number 5";
-        "${mod}+6" = "workspace number 6";
-        "${mod}+7" = "workspace number 7";
-        "${mod}+8" = "workspace number 8";
-        "${mod}+9" = "workspace number 9";
-        "${mod}+0" = "workspace number 10";
-
-        "${mod}+Shift+1" = "move container to workspace number 1";
-        "${mod}+Shift+2" = "move container to workspace number 2";
-        "${mod}+Shift+3" = "move container to workspace number 3";
-        "${mod}+Shift+4" = "move container to workspace number 4";
-        "${mod}+Shift+5" = "move container to workspace number 5";
-        "${mod}+Shift+6" = "move container to workspace number 6";
-        "${mod}+Shift+7" = "move container to workspace number 7";
-        "${mod}+Shift+8" = "move container to workspace number 8";
-        "${mod}+Shift+9" = "move container to workspace number 9";
-        "${mod}+Shift+0" = "move container to workspace number 10";
-
-        "${mod}+z" = "focus output left";
-        "${mod}+x" = "focus output right";
-
-        "${mod}+Shift+z" = "move output left";
-        "${mod}+Shift+x" = "move output right";
-
-        "${mod}+Tab" = "workspace back_and_forth";
-
-        "${mod}+Shift+s" = "exec swaylock -c 000000";
-
-        "XF86AudioRaiseVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+ && pkill -RTMIN+1 i3blocks";
-        "XF86AudioLowerVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%- && pkill -RTMIN+1 i3blocks";
-        "XF86AudioMute" = "exec wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle && pkill -RTMIN+1 i3blocks";
-        "XF86AudioMicMute" = "exec wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle && pkill -RTMIN+1 i3blocks";
-
-        "${mod}+b" = "splith";
-        "${mod}+v" = "splitv";
-        "${mod}+n" = "split none";
-
-        "${mod}+Shift+space" = "floating toggle";
-
-        "${mod}+r" = ''mode "resize"'';
-      };
-
-      modes = {
-        resize = with config.wayland.windowManager.sway.config; {
-          "${left}" = "resize shrink width 10px";
-          "${down}" = "resize grow height 10px";
-          "${up}" = "resize shrink height 10px";
-          "${right}" = "resize grow width 10px";
-
-          "Return" = ''mode "default"'';
-          "Escape" = ''mode "default"'';
-        };
-      };
-
-      output = {
-        "*" = {
-          background = "#000000 solid_color";
-        };
-
-        "eDP-1" = {
-          pos = "3840 1080";
-        };
-
-        "DP-3" = {
-          pos = "0 0";
-        };
-      };
-
-      input = {
-        "type:touchpad" = {
-          click_method = "button_areas";
-          natural_scroll = "disabled";
-          tap = "enabled";
-          dwt = "enabled";
-        };
-      };
-
-
-      startup = [
-        {
-          command = ''xsetroot -solid "#000000"'';
-        }
-      ];
     };
   };
 
